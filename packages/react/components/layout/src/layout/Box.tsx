c9cdedfd5555e0ac53ced4fd0e877a313873e149
qkdflrgs <qkdflrgs@gmail.com>
2024-08-02 15:31:33 +0900

    feat(box): add Box component

---
+++ packages/react/components/layout/src/layout/Box.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { BoxProps } from "./types";
+import { clsx } from "clsx";
+import { StyleSprinkles } from "../core/style.css";
+import { extractSprinkleProps } from "../utils/properties";
+import { vars } from "@litae/themes";
+
+const Box = (props: BoxProps, ref: React.Ref<HTMLElement>) => {
+  const { as = "div", color, background, children } = props;
+
+  return React.createElement(
+    as,
+    {
+      ...props,
+      ref,
+      style: {
+        color: vars.colors.$scale?.[color]?.[700] ?? color,
+        backgroundColor: vars.colors.$scale?.[background]?.[100] ?? background,
+        ...props.style,
+      },
+      className: clsx([
+        StyleSprinkles(
+          extractSprinkleProps(props, Array.from(StyleSprinkles.properties)),
+        ),
+        props.className,
+      ]),
+    },
+    children,
+  );
+};
+
+const _Box = React.forwardRef(Box);
+export { _Box as Box };
