f6281d1c7ce69e51b6a54844997cf153fb198ee4
qkdflrgs <qkdflrgs@gmail.com>
2024-08-02 17:17:13 +0900

    feat(typography): add Text component Storybook

---
+++ services/storybook/stories/ReactComponents/Layout/Text.stories.tsx
@@ -0,0 +1,36 @@
+import "@litae/react-components-layout/style.css";
+import { Text as _Text } from "@litae/react-components-layout";
+import { classes, vars } from "@litae/themes";
+
+export default {
+  title: "React Components/Layout/Typography/Text",
+  component: _Text,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    as: {
+      options: ["p", "span", "div", "b", "i", "u", "strong", "em"],
+      control: "select",
+    },
+    fontSize: {
+      options: Object.keys(classes.typography.text),
+      control: "select",
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: "select",
+    },
+  },
+};
+
+export const Text = {
+  args: {
+    as: "p",
+    children: "Hello World",
+    fontSize: "xl",
+    color: "gray",
+    background: "blue",
+  },
+};
