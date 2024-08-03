24e4ecf06b54b461c57c0f6565f1054991e8b1e1
qkdflrgs <qkdflrgs@gmail.com>
2024-08-03 10:50:03 +0900

    feat(grid): add Grid component

---
+++ packages/react/components/layout/src/layout/Grid.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { GridProps } from "./types";
+import { clsx } from "clsx";
+import { StyleSprinkles } from "../core/style.css";
+import { extractSprinkleProps } from "../utils/properties";
+import { vars } from "@litae/themes";
+
+const Grid = (props: GridProps, ref: React.Ref<HTMLElement>) => {
+  const {
+    as = "div",
+    color,
+    background,
+    autoColumns,
+    autoFlow,
+    autoRows,
+    column,
+    columnGap,
+    gap,
+    row,
+    rowGap,
+    templateAreas,
+    templateColumns,
+    templateRows,
+    children,
+  } = props;
+
+  return React.createElement(
+    as,
+    {
+      ...props,
+      ref,
+      style: {
+        display: "grid",
+        gridAutoColumns: autoColumns,
+        gridAutoFlow: autoFlow,
+        gridAutoRows: autoRows,
+        gridColumnGap: columnGap,
+        gridGap: gap,
+        gridRowGap: rowGap,
+        gridTemplateColumns: templateColumns,
+        gridTemplateRows: templateRows,
+        gridTemplateAreas: templateAreas,
+        gridColumn: column,
+        gridRow: row,
+        color: vars.colors.$scale?.[color]?.[700] ?? color,
+        backgroundColor: vars.colors.$scale?.[background]?.[100] ?? background,
+        ...props.style,
+      },
+      className: clsx([
+        StyleSprinkles(
+          extractSprinkleProps(props, Array.from(StyleSprinkles.properties)),
+        ),
+        props.className,
+      ]),
+    },
+    children,
+  );
+};
+
+const _Grid = React.forwardRef(Grid);
+
+export { _Grid as Grid };
