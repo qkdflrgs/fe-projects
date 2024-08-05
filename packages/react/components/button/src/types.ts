72eb5429c80af32772f67aaf5da84b1f60c201da
qkdflrgs <qkdflrgs@gmail.com>
2024-08-05 16:24:29 +0900

    refactr(button): add leftIcon, rightIcon props to Button component

--- packages/react/components/button/src/types.ts
+++ packages/react/components/button/src/types.ts
@@ -4,6 +4,8 @@ export type ButtonProps = {
   color?: keyof typeof vars.colors.$scale;
   isDisabled?: boolean;
   isLoading?: boolean;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
   size?: "xs" | "sm" | "md" | "lg";
   variant?: "solid" | "outline" | "ghost";
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
