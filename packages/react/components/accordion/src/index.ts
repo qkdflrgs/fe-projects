6aad2336a21074796a5ff2982b86387a5383d17c
qkdflrgs <qkdflrgs@gmail.com>
2024-08-06 16:50:21 +0900

    feat(accordion): export Accordion button component

---
+++ packages/react/components/accordion/src/index.ts
@@ -0,0 +1,8 @@
+export type {
+  AccordionProps,
+  AccordionItemProps,
+  AccordionButtonProps,
+} from "./types";
+export { Accordion } from "./Accordion";
+export { AccordionItem } from "./AccordionItem";
+export { AccordionButton } from "./AccordionButton";
