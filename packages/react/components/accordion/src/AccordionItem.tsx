01f0ca77afa39fd89206f7a3ce2b37ff4e1614ca
qkdflrgs <qkdflrgs@gmail.com>
2024-08-06 16:48:47 +0900

    feat(accordion): add Accordion item component

---
+++ packages/react/components/accordion/src/AccordionItem.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { Children, cloneElement } from "react";
+import { AccordionItemProps } from "./types";
+import { clsx } from "clsx";
+import { accordionItemStyle } from "./style.css";
+
+const AccordionItem = (
+  props: AccordionItemProps,
+  ref: React.Ref<HTMLDivElement>,
+) => {
+  const { itemName, children, className, ...rest } = props;
+
+  const childrenWithProps = Children.toArray(children);
+
+  const accordionItemChildren = childrenWithProps.map((child) => {
+    if (React.isValidElement(child)) {
+      return cloneElement(child, { ...child.props, itemName });
+    }
+
+    return null;
+  });
+
+  return (
+    <div ref={ref} className={clsx([accordionItemStyle, className])} {...rest}>
+      {accordionItemChildren}
+    </div>
+  );
+};
+
+const _AccordionItem = React.forwardRef(AccordionItem);
+
+export { _AccordionItem as AccordionItem };
