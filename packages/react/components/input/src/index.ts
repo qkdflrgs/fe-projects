5bd2a9c6c5acb0b7abe27c14d7c8bdd78f788af9
qkdflrgs <qkdflrgs@gmail.com>
2024-08-07 15:52:44 +0900

    feat(input): export InputLeftAddon component

--- packages/react/components/input/src/index.ts
+++ packages/react/components/input/src/index.ts
@@ -1,2 +1,4 @@
-export type { InputProps } from "./types";
+export type { InputProps, InputGroupProps, InputLeftAddonProps } from "./types";
 export { Input } from "./Input";
+export { InputGroup } from "./InputGroup";
+export { InputLeftAddon } from "./InputLeftAddon";
