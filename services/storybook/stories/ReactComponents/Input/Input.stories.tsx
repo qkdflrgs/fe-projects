756660394d4fcadc7d37ba6b42c8a5798aada791
qkdflrgs <qkdflrgs@gmail.com>
2024-08-07 15:53:15 +0900

    feat(input): add InputGroup to storybook

--- services/storybook/stories/ReactComponents/Input/Input.stories.tsx
+++ services/storybook/stories/ReactComponents/Input/Input.stories.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "@litae/react-components-input/style.css";
-import { Input } from "@litae/react-components-input";
+import {
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@litae/react-components-input";
 
 export default {
   title: "React Components/Input",
@@ -12,4 +16,13 @@ export default {
 
 export const InputStory = {
   render: () => <Input placeholder="dd" />,
+};
+
+export const InputGroupStory = {
+  render: () => (
+    <InputGroup size="lg" color="green">
+      <InputLeftAddon>$</InputLeftAddon>
+      <Input placeholder="내용을 입력해주세요" />
+    </InputGroup>
+  ),
 };
