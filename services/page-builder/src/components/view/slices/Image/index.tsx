1272ea204d02e078fe1f00963e5031bf65dd1799
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-08-13 01:36:08 +0900

    [Web] feat(hero): add Slices components(Text, Image) (#25)
    * feat(hero): add ImageSlice component
    
    * feat(hero): add TextSlice component
    
    * feat(hero): add Hero components to Playground page
    
    * feat(hero): change MobileFirstLayout component directory

---
+++ services/page-builder/src/components/view/slices/Image/index.tsx
@@ -0,0 +1,40 @@
+import { Box } from "@litae/react-components-layout";
+import { vars } from "@litae/themes";
+
+type Props = {
+  imageUrl: string;
+  alt: string;
+  sliceStyle?: {
+    width?: string;
+    padding?: keyof typeof vars.box.spacing;
+    paddingX?: keyof typeof vars.box.spacing;
+    paddingY?: keyof typeof vars.box.spacing;
+    backgroundColor?: string;
+  };
+};
+
+export const ImageSlice = ({ imageUrl, alt, sliceStyle }: Props) => {
+  const {
+    width = "100%",
+    padding = 0,
+    paddingX = 0,
+    paddingY = 0,
+    backgroundColor = "transparent",
+  } = sliceStyle ?? {};
+
+  return (
+    <Box
+      padding={padding}
+      paddingX={paddingX}
+      paddingY={paddingY}
+      style={{
+        width: "100%",
+        backgroundColor,
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <img src={imageUrl} alt={alt} style={{ width }} />
+    </Box>
+  );
+};
