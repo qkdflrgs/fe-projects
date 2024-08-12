75fffdc1e9a428117136246254c79816af456f9b
qkdflrgs <qkdflrgs@gmail.com>
2024-08-13 01:35:11 +0900

    feat(hero): change MobileFirstLayout component directory

--- services/page-builder/src/components/view/MobileFirstLayout/index.tsx
+++ services/page-builder/src/components/view/MobileFirstLayout/index.tsx
@@ -2,12 +2,10 @@ type Props = {
   children: React.ReactNode;
 };
 
-const MobileFirstLayout = ({ children }: Props) => {
+export const MobileFirstLayout = ({ children }: Props) => {
   return (
     <div className="w-screen h-screen flex justify-center bg-color-white dark:bg-color-black text-color-black dark:text-color-white">
       <div className="max-w-xl w-full">{children}</div>
     </div>
   );
 };
-
-export default MobileFirstLayout;
