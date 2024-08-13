21f46f6178e76d6bcad859dc6bfb0b91ecd0ff5a
qkdflrgs <qkdflrgs@gmail.com>
2024-08-13 17:50:33 +0900

    feat(section): add ImageSliderSectionSlice to Playground page

--- services/page-builder/src/pages/playground.tsx
+++ services/page-builder/src/pages/playground.tsx
@@ -4,10 +4,12 @@ import { TextSlice } from "../components/view/slices/Text";
 import { ImageSlice } from "../components/view/slices/Image";
 import { CDN_BASE_URL } from "../constants";
 import { SpacingSlice } from "../components/view/slices/Spacing";
+import { ImageSliderSectionSlice } from "../components/view/slices/ImageSliderSection";
 
 const PlaygroundPage = () => {
   return (
     <MobileFirstLayout>
+      {/* 히어로 영역 */}
       <TextSlice
         text="본 이벤트는 종료되었습니다 (~8/6)"
         sliceStyle={{
@@ -25,6 +27,8 @@ const PlaygroundPage = () => {
           backgroundColor: "#FFD951",
         }}
       />
+
+      {/* 혜택 영역 */}
       <SpacingSlice
         sliceStyle={{
           backgroundColor: vars.colors.$static.light.color.black,
@@ -54,7 +58,7 @@ const PlaygroundPage = () => {
       />
       <TextSlice
         text={
-          "100일간 내 마음대로 무제한 수강 가능한\n카테고리별 BEST 강의 라인업을 아래에서 확인해보세요!"
+          "원하는 강의를 골라 결제 시 100원만 더 내면\n이 모든 혜택을 드립니다!"
         }
         sliceStyle={{
           textSize: 14,
@@ -68,6 +72,126 @@ const PlaygroundPage = () => {
         sliceStyle={{
           paddingX: 8,
           backgroundColor: vars.colors.$static.light.color.black,
+        }}
+      />
+      {/* 강의 목록 영역 */}
+      <SpacingSlice
+        sliceStyle={{
+          backgroundColor: vars.colors.$scale.gray[900],
+          height: 60,
+        }}
+      />
+      <TextSlice
+        text={"100원에 만나볼 수 있는\nBEST 강의 라인업"}
+        highlightTexts={["BEST 강의"]}
+        sliceStyle={{
+          textSize: 24,
+          textWeight: 700,
+          textColor: vars.colors.$static.light.color.white,
+          backgroundColor: vars.colors.$scale.gray[900],
+          highlightTextColor: "#FFD64B",
+        }}
+      />
+      <TextSlice
+        text={
+          "100일간 내 마음대로 무제한 수강 가능한\n카테고리별 BEST 강의 라인업을 아래에서 확인해보세요!"
+        }
+        sliceStyle={{
+          textSize: 14,
+          textColor: vars.colors.$static.light.color.white,
+          backgroundColor: vars.colors.$scale.gray[900],
+        }}
+      />
+      <SpacingSlice
+        sliceStyle={{
+          backgroundColor: vars.colors.$scale.gray[900],
+          height: 32,
+        }}
+      />
+      <ImageSliderSectionSlice
+        text={"🖥️ 프로그래밍"}
+        images={[
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_programming_1.webp`,
+            alt: "강의 라인업 1 프로그래밍 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_programming_2.webp`,
+            alt: "강의 라인업 2 프로그래밍 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_programming_3.webp`,
+            alt: "강의 라인업 3 프로그래밍 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_programming_4.webp`,
+            alt: "강의 라인업 4 프로그래밍 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_programming_5.webp`,
+            alt: "강의 라인업 5 프로그래밍 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_programming_6.webp`,
+            alt: "강의 라인업 6 프로그래밍 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_programming_7.webp`,
+            alt: "강의 라인업 7 프로그래밍 강의",
+          },
+        ]}
+        sliceStyle={{
+          textColor: "#328FA1",
+          backgroundColor: vars.colors.$scale.gray[900],
+          paddingX: 8,
+        }}
+      />
+      <SpacingSlice
+        sliceStyle={{
+          backgroundColor: vars.colors.$scale.gray[900],
+          height: 32,
+        }}
+      />
+      <SpacingSlice
+        sliceStyle={{
+          backgroundColor: vars.colors.$scale.gray[900],
+          height: 32,
+        }}
+      />
+      <ImageSliderSectionSlice
+        text={"🖌️ 디자인/일러스트"}
+        images={[
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_design_1.webp`,
+            alt: "강의 라인업 1 디자인 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_design_2.webp`,
+            alt: "강의 라인업 2 디자인 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_design_3.webp`,
+            alt: "강의 라인업 3 디자인 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_design_4.webp`,
+            alt: "강의 라인업 4 디자인 강의",
+          },
+          {
+            imageUrl: `${CDN_BASE_URL}/test/lineup_design_5.webp`,
+            alt: "강의 라인업 5 디자인 강의",
+          },
+        ]}
+        sliceStyle={{
+          textColor: "#F26346",
+          backgroundColor: vars.colors.$scale.gray[900],
+          paddingX: 8,
+        }}
+      />
+      <SpacingSlice
+        sliceStyle={{
+          backgroundColor: vars.colors.$scale.gray[900],
+          height: 32,
         }}
       />
     </MobileFirstLayout>
