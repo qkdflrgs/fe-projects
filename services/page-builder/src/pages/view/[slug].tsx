8c3fdd2e8ac75ae9eae3f5c3bd6aa269f06b1607
qkdflrgs <qkdflrgs@gmail.com>
2024-08-14 16:54:51 +0900

    feat(editor): refactoring View page with useViewSchemaSlices hook

--- services/page-builder/src/pages/view/[slug].tsx
+++ services/page-builder/src/pages/view/[slug].tsx
@@ -1,78 +1,24 @@
 import { MobileFirstLayout } from "@/src/components/layout/MobileFirstLayout";
-import { AccordionSlice } from "@/src/components/view/slices/Accordion";
-import { ImageSlice } from "@/src/components/view/slices/Image";
-import { ImageSliderSectionSlice } from "@/src/components/view/slices/ImageSliderSection";
-import { SpacingSlice } from "@/src/components/view/slices/Spacing";
-import { TextSlice } from "@/src/components/view/slices/Text";
-import { useMemo } from "react";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import { CDN_BASE_URL } from "@/src/constants";
 import {
-  MetaDataSlice,
-  MetaDataSliceProps,
-} from "@/src/components/view/slices/MetaData";
-
-type Schema = {
-  id: string;
-  slug: string;
-  metadata?: MetaDataSliceProps;
-  slices: {
-    sliceName:
-      | "TextSlice"
-      | "ImageSlice"
-      | "SpacingSlice"
-      | "ImageSliderSectionSlice"
-      | "AccordionSlice";
-    data: any;
-  }[];
-};
+  useViewSchemaSlices,
+  ViewSchema,
+} from "@/src/hooks/useViewSchemaSlices";
 
 const ViewPage = ({
   jsonSchema,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const slices = useMemo(() => {
-    const sliceList = [] as React.ReactNode[];
-
-    if (jsonSchema.metadata) {
-      sliceList.push(<MetaDataSlice {...jsonSchema.metadata} />);
-    }
-
-    jsonSchema.slices.forEach(({ sliceName, data }) => {
-      switch (sliceName) {
-        case "TextSlice": {
-          sliceList.push(<TextSlice {...data} />);
-          break;
-        }
-        case "ImageSlice": {
-          sliceList.push(<ImageSlice {...data} />);
-          break;
-        }
-        case "SpacingSlice": {
-          sliceList.push(<SpacingSlice {...data} />);
-          break;
-        }
-        case "ImageSliderSectionSlice": {
-          sliceList.push(<ImageSliderSectionSlice {...data} />);
-          break;
-        }
-        case "AccordionSlice": {
-          sliceList.push(<AccordionSlice {...data} />);
-          break;
-        }
-      }
-    });
-
-    return sliceList;
-  }, []);
+  const slices = useViewSchemaSlices(jsonSchema);
 
   return <MobileFirstLayout>{slices}</MobileFirstLayout>;
 };
 
 export default ViewPage;
 
-export const getStaticProps: GetStaticProps<{ jsonSchema: Schema }> = async (
-  context,
-) => {
+export const getStaticProps: GetStaticProps<{
+  jsonSchema: ViewSchema;
+}> = async (context) => {
   const slug = (context.params?.slug as string) ?? "";
 
   const sliceSlug = slug.split("-");
