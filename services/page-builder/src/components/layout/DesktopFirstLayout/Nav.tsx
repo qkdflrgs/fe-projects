273c277dfda0471385875f11c5e575bc2e244ccb
qkdflrgs <qkdflrgs@gmail.com>
2024-08-14 13:22:54 +0900

    feat(desktop): add DesktopFirstNav component

---
+++ services/page-builder/src/components/layout/DesktopFirstLayout/Nav.tsx
@@ -0,0 +1,34 @@
+import { Flex, FlexProps } from "@litae/react-components-layout";
+import { vars } from "@litae/themes";
+
+type Props = FlexProps;
+
+export const DesktopFirstNav = (props: Props) => {
+  const {
+    className,
+    padding = 2,
+    boxShadow = "base",
+    justify = "right",
+    children,
+    ...rest
+  } = props;
+
+  const currentClassName = ["w-full h-[56px] fixed top-0 z-50", className].join(
+    " ",
+  );
+
+  return (
+    <Flex
+      {...rest}
+      padding={padding}
+      boxShadow={boxShadow}
+      justify={justify}
+      className={currentClassName}
+      style={{
+        backgroundColor: vars.colors.$static.light.color.white,
+      }}
+    >
+      {children}
+    </Flex>
+  );
+};
