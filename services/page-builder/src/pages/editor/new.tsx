0f9b07fb1b047e855b59b4fc26b400379e0f29ff
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-08-14 15:53:22 +0900

    [Web] feat(editor): add Json Editor(based @monaco-editor/react) (#29)
    * feat(editor): install @monaco-editor/react to @litae/page-builder
    
    * feat(editor): add View schema snippet
    
    * feat(editor): add JsonEditor component
    
    * feat(editor): add formatObjectToJson function
    
    * feat(editor): add JsonEditor to Editor new page

--- services/page-builder/src/pages/editor/new.tsx
+++ services/page-builder/src/pages/editor/new.tsx
@@ -2,12 +2,24 @@ import { Button } from "@litae/react-components-button";
 import { DesktopFirstLayout } from "@/src/components/layout/DesktopFirstLayout";
 import { DesktopFirstBody } from "@/src/components/layout/DesktopFirstLayout/Body";
 import { DesktopFirstNav } from "@/src/components/layout/DesktopFirstLayout/Nav";
+import { JsonEditor } from "@/src/components/Editor/Json";
+import { useState } from "react";
+import { ViewSliceSchemaSnippet } from "@/src/utils/jsonEditor/ViewSchemaSnippet";
+import { formatObjectToJson } from "@/src/components/Editor";
 
 const EditorNewPage = () => {
+  const [schema, setSchema] = useState(
+    formatObjectToJson(ViewSliceSchemaSnippet.init),
+  );
+
+  const handleReset = () => {
+    setSchema(formatObjectToJson(ViewSliceSchemaSnippet.init));
+  };
+
   return (
     <DesktopFirstLayout>
       <DesktopFirstNav gap={8}>
-        <Button variant="outline" size="md" color="red">
+        <Button variant="outline" size="md" color="red" onClick={handleReset}>
           초기화
         </Button>
         <Button variant="outline" size="md" color="gray">
@@ -17,7 +29,12 @@ const EditorNewPage = () => {
           배포하기
         </Button>
       </DesktopFirstNav>
-      <DesktopFirstBody>/editor/new</DesktopFirstBody>
+      <DesktopFirstBody>
+        <JsonEditor
+          value={schema}
+          onChange={(value) => setSchema(value || "")}
+        />
+      </DesktopFirstBody>
     </DesktopFirstLayout>
   );
 };
