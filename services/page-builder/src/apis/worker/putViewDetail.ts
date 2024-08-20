522977c8d9418089be4f9887937fce3999b93fe1
qkdflrgs <qkdflrgs@gmail.com>
2024-08-20 12:32:24 +0900

    feat(api): fix putViewDetail api

--- services/page-builder/src/apis/worker/putViewDetail.ts
+++ services/page-builder/src/apis/worker/putViewDetail.ts
@@ -14,7 +14,5 @@ type Params = {
 };
 
 export const putViewDetail = async ({ viewId, data }: Params) => {
-  const response = await workerInstance.put(getPutViewDetailPath(viewId), data);
-
-  console.log(response);
+  await workerInstance.put(getPutViewDetailPath(viewId), data);
 };
