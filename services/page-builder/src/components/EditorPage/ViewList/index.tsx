2db6375197d65e14cd780715724499378a8b1a66
qkdflrgs <qkdflrgs@gmail.com>
2024-08-20 13:24:18 +0900

    feat(list): add ViewList component

---
+++ services/page-builder/src/components/EditorPage/ViewList/index.tsx
@@ -0,0 +1,54 @@
+import { ViewKeyData } from "@/src/apis/worker/getViewList";
+import { formatDate } from "@/src/utils/date/format";
+import { Box, Text, Divider } from "@litae/react-components-layout";
+import { vars } from "@litae/themes";
+
+type Props = {
+  viewList: ViewKeyData[];
+};
+
+export const ViewList = ({ viewList }: Props) => {
+  const sortedLastedDateViewList = [
+    ...viewList.sort((current, prev) => {
+      const currentDate = new Date(current.metadata.createAt);
+      const prevDate = new Date(prev.metadata.createAt);
+
+      return prevDate.getTime() - currentDate.getTime();
+    }),
+  ];
+
+  return (
+    <Box
+      marginY={14}
+      paddingX={8}
+      paddingY={6}
+      className="max-w-[600px] w-full"
+      boxShadow="base"
+      style={{
+        background: vars.colors.$static.light.color.white,
+      }}
+    >
+      <ul>
+        {sortedLastedDateViewList.map(({ name, metadata }) => (
+          <a key={name} href={`/view/${name}`} target="_blank" rel="noreferrer">
+            <li className="p-2 hover:bg-gray-100">
+              <Text
+                fontSize="sm"
+                style={{ fontWeight: vars.typography.fontWeight[600] }}
+              >
+                {metadata.title ?? name}
+              </Text>
+              <Text
+                fontSize="xs"
+                style={{ color: vars.colors.$static.light.gray[500] }}
+              >
+                {formatDate(metadata.createAt)}
+              </Text>
+            </li>
+            <Divider />
+          </a>
+        ))}
+      </ul>
+    </Box>
+  );
+};
