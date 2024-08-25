ff19c141407906171376b13c564d602509d7d2c5
qkdflrgs <qkdflrgs@gmail.com>
2024-08-26 00:31:02 +0900

    feat(form): add preview and publish function to Editor new form page

--- services/page-builder/src/pages/editor/new/index.tsx
+++ services/page-builder/src/pages/editor/new/index.tsx
@@ -1,17 +1,101 @@
+import { putViewDetail } from "@/src/apis/worker/putViewDetail";
+import { formatObjectToJson } from "@/src/components/Common/Editor";
 import { InputField } from "@/src/components/Common/Form/Field/InputField";
 import { FormFieldSection } from "@/src/components/Common/Form/Layouts/FormFieldSection";
 import { DesktopFirstLayout } from "@/src/components/Common/Layouts/DesktopFirstLayout";
 import { DesktopFirstBody } from "@/src/components/Common/Layouts/DesktopFirstLayout/Body";
 import { DesktopFirstNav } from "@/src/components/Common/Layouts/DesktopFirstLayout/Nav";
 import { DesktopFirstSideNav } from "@/src/components/Common/Layouts/DesktopFirstLayout/SideNav";
+import { Spacing } from "@/src/components/Common/Spacing";
+import { ViewSliceSchemaSnippet } from "@/src/utils/jsonEditor/ViewSchemaSnippet";
+import { previewStorage } from "@/src/utils/storage";
+import { getValidateFormErrorMessage } from "@/src/utils/validation/error";
+import { ViewSchemaProps } from "@/src/utils/validation/schema/types";
+import { ViewSchema } from "@/src/utils/validation/schema/view";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@litae/react-components-button";
 import { Box, Flex } from "@litae/react-components-layout";
+import { useToast } from "@litae/react-components-toast";
 import { vars } from "@litae/themes";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import ShortUniqueId from "short-unique-id";
 
 const EditorNewFormPage = () => {
-  const handleReset = () => {};
-  const handlePreview = () => {};
-  const handlePublish = () => {};
+  const { randomUUID } = new ShortUniqueId({ length: 10 });
+  const [viewId] = useState(randomUUID());
+  const { toast } = useToast();
+  const { register, handleSubmit, reset } = useForm<ViewSchemaProps>({
+    defaultValues: ViewSliceSchemaSnippet.init,
+    resolver: zodResolver(ViewSchema),
+  });
+
+  const handleReset = () => {
+    reset();
+  };
+  const handlePreview = handleSubmit(
+    (formData) => {
+      previewStorage.set(viewId, formatObjectToJson(formData));
+
+      window.open(`/preview/${viewId}`, "_blank");
+    },
+    (formError) => {
+      const errors = getValidateFormErrorMessage(formError);
+      const firstError = errors[0];
+
+      if (firstError) {
+        return toast({
+          payload: {
+            message: `[${firstError.key}] ${firstError.message}`,
+          },
+        });
+      }
+    },
+  );
+
+  const handlePublish = handleSubmit(
+    async (formData) => {
+      const convertedSlug = formData.slug.split(" ").join("-");
+      const currentFormData = {
+        ...formData,
+        slug: `${convertedSlug}-${viewId}`,
+      };
+
+      try {
+        await putViewDetail({
+          viewId,
+          data: {
+            value: formatObjectToJson(currentFormData),
+            metadata: {
+              title: formData.slug,
+              createAt: new Date().toISOString(),
+            },
+          },
+        });
+
+        window.open(`/view/${currentFormData.slug}`, "_blank");
+      } catch (error) {
+        toast({
+          payload: {
+            // @ts-ignore
+            message: `[Fetch Error] ${error.message}`,
+          },
+        });
+      }
+    },
+    (formError) => {
+      const errors = getValidateFormErrorMessage(formError);
+      const firstError = errors[0];
+
+      if (firstError) {
+        return toast({
+          payload: {
+            message: `[${firstError.key}] ${firstError.message}`,
+          },
+        });
+      }
+    },
+  );
 
   return (
     <DesktopFirstLayout>
@@ -45,8 +129,17 @@ const EditorNewFormPage = () => {
             boxShadow="base"
             style={{ backgroundColor: vars.colors.$static.light.color.white }}
           >
-            <FormFieldSection title="test">
-              <InputField isRequired label="test" />
+            <FormFieldSection title="common">
+              <InputField isRequired label="slug" {...register("slug")} />
+            </FormFieldSection>
+            <Spacing />
+            <FormFieldSection title="metadata">
+              <InputField label="title" {...register("metadata.title")} />
+              <InputField label="ogTitle" {...register("metadata.ogTitle")} />
+              <InputField
+                label="ogDescription"
+                {...register("metadata.ogDescription")}
+              />
             </FormFieldSection>
           </Box>
         </Flex>
