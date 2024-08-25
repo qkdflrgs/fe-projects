be869d8221b405404a239e088b0fcdb6ee4d35d6
qkdflrgs <qkdflrgs@gmail.com>
2024-08-26 01:44:10 +0900

    refactor(new): add EditorNewFormSideNavBar component

---
+++ services/page-builder/src/components/Features/EditorNewFormPage/SideNavBar/index.tsx
@@ -0,0 +1,5 @@
+import { DesktopFirstSideNav } from "@/src/components/Common/Layouts/DesktopFirstLayout/SideNav";
+
+export const EditorNewFormSideNavBar = () => {
+  return <DesktopFirstSideNav>사이드 네비게이션 바</DesktopFirstSideNav>;
+};
