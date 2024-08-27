ac2b2b6d19a11f3f0ba224108500ea5278361e5f
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-08-27 15:49:14 +0900

    [Web] feat(image field): add ImageField (#41)
    * feat(imageField): install @aws-sdk/client-s3
    
    * feat(imageField): install @aws-sdk/s3-request-presigner
    
    * feat(imageField): add env files to gitignore
    
    * feat(imageField): add R2 Instance
    
    * feat(imageField): add putImageFile to apis
    
    * feat(imageField): add ImageURLField component
    
    * feat(imageField): add ImageURLInput component
    
    * feat(imageField): add ImageSlice to EditorNewFormSideNavBar presets
    
    * feat(imageField): fix padding types of commonSliceStyleSchema
    
    * feat(imageField): add ViewSchemaFormSliceImageFields
    
    * feat(imageField): add ImageSlice to ViewSchemaFormSliceFields

---
+++ services/page-builder/src/apis/r2/putImageFile.ts
@@ -0,0 +1,34 @@
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { r2Instance } from ".";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import axios from "axios";
+import { CDN_BASE_URL } from "@/src/constants";
+
+type Params = {
+  file: File;
+  fileName: string;
+};
+
+export const putImageFile = async ({ file, fileName }: Params) => {
+  const key = `images/${fileName}`;
+
+  try {
+    const url = await getSignedUrl(
+      r2Instance,
+      new PutObjectCommand({
+        Bucket: "litae-page-builder",
+        Key: key,
+        Body: file,
+        ContentType: file.type,
+        ACL: "public-read",
+      }),
+      { expiresIn: 3600 },
+    );
+
+    await axios.put(url, file);
+
+    return `${CDN_BASE_URL}/${key}`;
+  } catch {
+    throw new Error("이미지 업로드에 실패했습니다");
+  }
+};
