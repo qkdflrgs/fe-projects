19569405a22c243994d4a48db4c0016b87cbacc9
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-08-29 03:45:54 +0900

    [Web] feat(detail): add Swap function to Slice fields (#45)
    * feat(detail): add hideSlice type to SliceSchemas
    
    * feat(detail): add hide function to SliceFieldTitleNavBar
    
    * feat(detail): add hide function to useViewSchemaSlices
    
    * feat(detail): add swap function to SliceFields
    
    * feat(detail): add types of ViewMetadata(isDraft, updatedAt)
    
    * feat(detail): add metadata(isDraft, updatedAt) to Editor new page's handlePublish
    
    * feat(detail): add draft page logic to ViewPage
    
    * feat(detail): add handlePublish to ViewList

--- services/page-builder/src/components/Features/EditorNewFormPage/ViewSchemaForm/SliceFields/Common/SliceFieldTitleNavBar.tsx
+++ services/page-builder/src/components/Features/EditorNewFormPage/ViewSchemaForm/SliceFields/Common/SliceFieldTitleNavBar.tsx
@@ -1,3 +1,4 @@
+import { useViewSchemaFormContext } from "@/src/hooks/useViewSchemaForm";
 import { useViewSchemaFormSliceFieldArray } from "@/src/hooks/useViewSchemaFormSliceFieldArray";
 import { Button } from "@litae/react-components-button";
 import { Box, Flex } from "@litae/react-components-layout";
@@ -15,6 +16,15 @@ export const SliceFieldTitleNavBar = ({
   appendRight,
 }: Props) => {
   const { remove } = useViewSchemaFormSliceFieldArray();
+  const { watch, setValue } = useViewSchemaFormContext();
+
+  const hideSlice = watch(`slices.${fieldIndex}.hideSlice`);
+  const handleHideToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
+    setValue(`slices.${fieldIndex}.hideSlice`, !hideSlice);
+  };
+
   const handleRemove = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
 
@@ -27,7 +37,15 @@ export const SliceFieldTitleNavBar = ({
       {appendRight ? (
         <Box>{appendRight}</Box>
       ) : (
-        <Box>
+        <Flex gap={6}>
+          <Button
+            size="xs"
+            variant="outline"
+            color={hideSlice ? "gray" : "blue"}
+            onClick={handleHideToggle}
+          >
+            {hideSlice ? "미노출" : "노출"}
+          </Button>
           <Button
             size="xs"
             variant="outline"
@@ -36,7 +54,7 @@ export const SliceFieldTitleNavBar = ({
           >
             삭제
           </Button>
-        </Box>
+        </Flex>
       )}
     </Flex>
   );
