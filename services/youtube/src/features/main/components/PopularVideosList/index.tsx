046f14b849b074117e2eff7b5619e82dee7882fc
qkdflrgs <qkdflrgs@gmail.com>
2024-09-02 13:32:10 +0900

    feat(list): add data fetching by useGetPopularVideosList to PopularVideosList

--- services/youtube/src/features/main/components/PopularVideosList/index.tsx
+++ services/youtube/src/features/main/components/PopularVideosList/index.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import { vars } from "@litae/themes";
-import { getPopularVideosList } from "@/src/features/main/api/getPopularVideosList";
+import { useGetPopularVideosList } from "@/src/features/main/hooks/useGetPopularVideosList";
+import { useEffect } from "react";
 
 export const PopularVideosList = async () => {
-  const data = await getPopularVideosList({});
+  const { data, fetchNextPage, isFetchingNextPage } = useGetPopularVideosList(
+    {},
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchNextPage();
+    }, 2000);
+  }, []);
+
+  console.log(data);
 
   return (
     <section style={{ color: vars.colors.$scale.gray[900] }}>
       <h2>목록</h2>
       <br />
-      <p>{data.lists?.[0].title}</p>
+      <p>fetching: {isFetchingNextPage ? "true" : "false"}</p>
+      <br />
+      <p>{data.pages?.[0].lists?.[0].title}</p>
     </section>
   );
 };
