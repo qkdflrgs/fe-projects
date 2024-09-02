d1deb82f29280168b96b6ed80fa333cf828aae88
qkdflrgs <qkdflrgs@gmail.com>
2024-09-02 12:07:37 +0900

    feat(api): add API for get popular videos list

--- services/youtube/src/features/main/api/getPopularVideosList.ts
+++ services/youtube/src/features/main/api/getPopularVideosList.ts
@@ -1,6 +1,8 @@
+import { API_BASE_URL } from "@/src/shared/api/constants";
 import { VideoThumbnail } from "@/src/shared/api/youtube/types/item";
 import { ListPageApiInfo } from "@/src/shared/api/youtube/types/list";
 import { youtube_v3 } from "googleapis";
+import queryString from "query-string";
 
 export type GetPopularVideosListRequestParams = Pick<
   youtube_v3.Params$Resource$Videos$List,
@@ -23,3 +25,17 @@ export type PopularListItem = {
 export type GetPopularVideosListResponse = {
   lists: PopularListItem[];
 } & ListPageApiInfo;
+
+export const getPopularVideosListPath = "/api/videos/popular-list";
+
+export const getPopularVideosList = async (
+  params: GetPopularVideosListRequestParams,
+): Promise<GetPopularVideosListResponse> => {
+  const queryParams = queryString.stringify(params);
+
+  const url = `${API_BASE_URL}${getPopularVideosListPath}?${queryParams}`;
+
+  const response = await fetch(url);
+
+  return await response.json();
+};
