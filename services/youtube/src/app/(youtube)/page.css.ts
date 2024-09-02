5e3ef69ece2ffa38b9b76cee496e4a0f45933a86
qkdflrgs <qkdflrgs@gmail.com>
2024-09-02 12:07:12 +0900

    feat(api): add style of Home page

---
+++ services/youtube/src/app/(youtube)/page.css.ts
@@ -0,0 +1,14 @@
+import { style } from "@vanilla-extract/css";
+import { f } from "@/src/shared/styles/functions";
+import { vars } from "@litae/themes";
+
+export const main = style([
+  f.flexCenterBox,
+  f.directionColumn,
+  f.hScreen,
+  f.wScreen,
+  f.color.scale.gray[900],
+  {
+    backgroundColor: vars.colors.$scale.gray[50],
+  },
+]);
