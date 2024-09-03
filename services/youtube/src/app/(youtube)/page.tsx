ce2615486734069e483d27ca6b524bc4e0d2ae29
qkdflrgs <qkdflrgs@gmail.com>
2024-09-03 15:56:01 +0900

    feat(main): clean up Home page

--- services/youtube/src/app/(youtube)/page.tsx
+++ services/youtube/src/app/(youtube)/page.tsx
@@ -1,11 +1,9 @@
 import { Suspense } from "react";
-import * as s from "../(youtube)/page.css";
 import { PopularVideosList } from "@/src/features/main/components/PopularVideosList";
 
 export default function Home() {
   return (
-    <main className={s.main}>
-      <h1 style={{ padding: "12px" }}>타이틀</h1>
+    <main>
       <Suspense fallback={<div>🥲</div>}>
         <PopularVideosList />
       </Suspense>
