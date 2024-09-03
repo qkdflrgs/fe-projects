ca4b7de7467adbff069be78c9a2b8be87f223b99
qkdflrgs <qkdflrgs@gmail.com>
2024-09-03 23:10:50 +0900

    feat(main): add VisibilityLoader to PopularVideosList

--- services/youtube/src/features/main/components/PopularVideosList/index.tsx
+++ services/youtube/src/features/main/components/PopularVideosList/index.tsx
@@ -1,19 +1,30 @@
 "use client";
-
 import { PopularVideosListItem } from "./ListItem";
 import * as s from "./style.css";
 import { useGetPopularVideosList } from "@/src/features/main/hooks/useGetPopularVideosList";
+import { VisibilityLoader } from "@/src/shared/components/VisibilityLoader";
 
-export const PopularVideosList = async () => {
-  const { data } = useGetPopularVideosList({});
+export const PopularVideosList = () => {
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useGetPopularVideosList({});
 
   const flatData = data.pages.map((page) => page?.lists ?? []).flat();
 
   return (
-    <section className={s.wrapper}>
-      {flatData.map((item) => (
-        <PopularVideosListItem key={item.videoId} video={item} />
-      ))}
-    </section>
+    <>
+      <section className={s.wrapper}>
+        {flatData.map((item) => (
+          <PopularVideosListItem key={item.videoId} video={item} />
+        ))}
+      </section>
+
+      {hasNextPage && (
+        <VisibilityLoader
+          callback={() => {
+            !isFetchingNextPage && fetchNextPage();
+          }}
+        />
+      )}
+    </>
   );
 };
