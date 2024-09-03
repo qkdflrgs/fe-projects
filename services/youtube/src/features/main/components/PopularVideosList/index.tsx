ca28ccbe8d939a6226deaef2105ccc34a1882191
qkdflrgs <qkdflrgs@gmail.com>
2024-09-03 15:50:18 +0900

    feat(main): add PopularVideosListItem to PopularVideosList

--- services/youtube/src/features/main/components/PopularVideosList/index.tsx
+++ services/youtube/src/features/main/components/PopularVideosList/index.tsx
@@ -1,27 +1,19 @@
 "use client";
 
-import { vars } from "@litae/themes";
+import { PopularVideosListItem } from "./ListItem";
+import * as s from "./style.css";
 import { useGetPopularVideosList } from "@/src/features/main/hooks/useGetPopularVideosList";
-import { useEffect } from "react";
 
 export const PopularVideosList = async () => {
-  const { data, fetchNextPage, isFetchingNextPage } = useGetPopularVideosList(
-    {},
-  );
+  const { data } = useGetPopularVideosList({});
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetchNextPage();
-    }, 2000);
-  }, []);
+  const flatData = data.pages.map((page) => page?.lists ?? []).flat();
 
   return (
-    <section style={{ color: vars.colors.$scale.gray[900] }}>
-      <h2>목록</h2>
-      <br />
-      <p>fetching: {isFetchingNextPage ? "true" : "false"}</p>
-      <br />
-      <p>{data.pages?.[0].lists?.[0].title}</p>
+    <section className={s.wrapper}>
+      {flatData.map((item) => (
+        <PopularVideosListItem key={item.videoId} video={item} />
+      ))}
     </section>
   );
 };
