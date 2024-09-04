9ad6d83637207f778bad0dda16a4a1bd18f7c3b9
qkdflrgs <qkdflrgs@gmail.com>
2024-09-05 02:54:43 +0900

    feat(detail): add commentCount to Video detail api

--- services/youtube/src/app/api/videos/detail/[videoId]/route.ts
+++ services/youtube/src/app/api/videos/detail/[videoId]/route.ts
@@ -57,6 +57,7 @@ const mappingResponse = (
   const videoParsedViewCount = parseInt(videoData.statistics?.viewCount ?? "0");
   const videoLikeCount = parseInt(videoData.statistics?.likeCount ?? "0");
   const videoDislikeCount = parseInt(videoData.statistics?.dislikeCount ?? "0");
+  const videoCommentCount = parseInt(videoData.statistics?.commentCount ?? "0");
 
   const channelViewCount = parseInt(channelData.statistics?.viewCount ?? "0");
   const channelSubscriberCount = parseInt(
@@ -88,6 +89,11 @@ const mappingResponse = (
       dislikeCount: videoDislikeCount,
       dislikeCountDisplayText: formatNumberToKoreanText(
         videoDislikeCount,
+        true,
+      ),
+      commentCount: videoCommentCount,
+      commentCountDisplayText: formatNumberToKoreanText(
+        videoCommentCount,
         true,
       ),
       channelInfo: {
