fc2b8dad703850083169e72738c4a183e6a2f4ca
qkdflrgs <qkdflrgs@gmail.com>
2024-09-05 01:07:32 +0900

    feat(detail): add getMockVideosDetail to MSW handlers

--- services/youtube/src/shared/mocks/handler.ts
+++ services/youtube/src/shared/mocks/handler.ts
@@ -1,8 +1,10 @@
 import { getMockPopularVideosList } from "@/src/features/main/api/getPopularVideosList.mock";
 import { getMockSearchVideosList } from "@/src/features/search/api/getSearchVideosList.mock";
+import { getMockVideosDetail } from "@/src/features/videos/detail/api/getVideosDetail.mock";
 import { HttpHandler } from "msw";
 
 export const handlers: HttpHandler[] = [
   getMockPopularVideosList,
   getMockSearchVideosList,
+  getMockVideosDetail,
 ];
