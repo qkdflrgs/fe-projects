6ef75938f6abb68a4d68faf5c8f214880be03f99
qkdflrgs <qkdflrgs@gmail.com>
2024-09-05 02:57:53 +0900

    feat(detail): add VideoDetail page

--- services/youtube/src/app/(youtube)/videos/detail/[videoId]/page.tsx
+++ services/youtube/src/app/(youtube)/videos/detail/[videoId]/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import * as s from "./style.css";
 import { VideoDetailPageParams } from "@/src/features/videos/detail/types";
 import { VideoDetail } from "@/src/features/videos/detail/components/VideoDetail";
+import { VideoDetailCommentList } from "@/src/features/videos/detail/components/VideoDetailCommentList";
 
 export default function VideoDetailPage({ params }: VideoDetailPageParams) {
   const { videoId } = params;
@@ -12,6 +13,9 @@ export default function VideoDetailPage({ params }: VideoDetailPageParams) {
         <div className={s.wrapper}>
           <Suspense fallback={<></>}>
             <VideoDetail videoId={videoId} />
+          </Suspense>
+          <Suspense fallback={<></>}>
+            <VideoDetailCommentList videoId={videoId} />
           </Suspense>
         </div>
       </div>
