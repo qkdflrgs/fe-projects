b41b9321aa9410f8fa435cc9c19c094fc0a74eda
qkdflrgs <qkdflrgs@gmail.com>
2024-09-04 18:43:26 +0900

    feat(search): add page link utils functions

---
+++ services/youtube/src/shared/utils/link/page.ts
@@ -0,0 +1,34 @@
+import { GetSearchVideosListRequestParams } from "@/src/features/search/api/getSearchVideosList";
+import queryString from "query-string";
+
+type SearchPageQueryParams = Pick<
+  GetSearchVideosListRequestParams,
+  "order" | "q"
+>;
+
+export const getSearchPageLink = (params: SearchPageQueryParams) => {
+  const parsedParams = queryString.parse(location.search);
+
+  if (params.q) {
+    parsedParams.q = params.q;
+  }
+
+  if (params.order) {
+    parsedParams.order = params.order;
+  }
+
+  return `/search?${queryString.stringify(parsedParams)}`;
+};
+
+export const getMainPageLink = () => {
+  return "/";
+};
+
+type VideoDetailPageParams = {
+  videoId: string;
+};
+export const getVideoDetailPageLink = (params: VideoDetailPageParams) => {
+  console.log(params);
+
+  return "/";
+};
