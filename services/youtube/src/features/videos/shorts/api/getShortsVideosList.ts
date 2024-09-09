e740441ee75e24e2e0e26b9ff2775cfe8794354e
qkdflrgs <qkdflrgs@gmail.com>
2024-09-09 16:47:50 +0900

    feat(shortsComment): add getShortsVideosList for fetching shorts video list

---
+++ services/youtube/src/features/videos/shorts/api/getShortsVideosList.ts
@@ -0,0 +1,25 @@
+import { API_BASE_URL } from "@/src/shared/api/constants";
+import { ListResponse } from "@/src/shared/api/youtube/types/list";
+import { youtube_v3 } from "googleapis";
+import queryString from "query-string";
+
+export type GetShortsVideosListRequestParams = Pick<
+  youtube_v3.Params$Resource$Search$List,
+  "pageToken"
+>;
+
+export type GetShortsVideosListResponse = ListResponse<{ videoId: string }>;
+
+export const getShortsVideosListUrl = `${API_BASE_URL}/api/videos/shorts`;
+
+export const getShortsVideosList = async (
+  params: GetShortsVideosListRequestParams,
+) => {
+  const queryParams = queryString.stringify(params);
+
+  const url = `${getShortsVideosListUrl}?${queryParams}`;
+
+  const response = await fetch(url);
+
+  return await response.json();
+};
